25744b7fd763006496add23f59a1f0e4ea04968f
JunHoJang <jjjh3369@naver.com>
2023-12-12 14:59:08 +0900

    10

--- HTML/js/openCloseHosChart.js
+++ HTML/js/openCloseHosChart.js
@@ -1,45 +1,16 @@
+var data = {
+	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+	datasets: [{
+		label: 'BigData Analysis',
+		data: [65, 59, 80, 81, 56, 55, 40],
+		backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
+	}]
+};
 var ctx = document.getElementById('openCloseHosChart').getContext('2d');
 var openCloseHosChart = new Chart(ctx, {
-	type: 'line',
-	data: {
-		labels: ['Label 1', 'Label 2', 'Label 3'], // X축 레이블
-		datasets: [{
-			label: '은평구',
-			data: [10, 20, 30], // 첫 번째 라인의 데이터
-			borderColor: 'blue', // 첫 번째 라인의 색상
-			borderWidth: 1
-		}, {
-			label: '서대문구',
-			data: [20, 30, 45], // 두 번째 라인의 데이터
-			borderColor: 'red', // 두 번째 라인의 색상
-			borderWidth: 1
-		}, {
-			label: '마포구',
-			data: [15, 25, 35], // 두 번째 라인의 데이터
-			borderColor: 'black', // 두 번째 라인의 색상
-			borderWidth: 1
-		},
-		]
-	},
-	options: {
-		plugins: {
-			legend: {
-				labels: {
-					font: {
-						size: 20,
-						family: 'Noto Sans KR'
-					}
-				}
-			}
-		},scales: {
-			y: {
-				font: {
-					size: 20,
-					family: 'Noto Sans KR'
-				}
-			}
-	}
-	}
+	type: 'bar',
+	data: data,
+	options: {}
 });
 
 function resizeChart() {
